2f359eb4e513ea97dec29aa70525133506e66b1e
craigvdodge <craigvdodge@gmail.com>
2022-03-29 16:48:24 -0700

    Take wordlist from source, sort it alphabetically, capitalize, and deduplicate.

--- helpleDataProc/Program.cs
+++ helpleDataProc/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Helple Data Processor");
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: helpleDataProc input-file output-file");
+    return;
+}
 
-// remove when done debugging
-Console.ReadKey();
+string[] words = File.ReadAllLines(args[0]);
+
+// Write to Sorted list to 1.order and 2.deduplicate
+SortedList<string, int> outputList = new SortedList<string, int>(words.Length);
+
+foreach (string word in words)
+{
+    if (word.Length != 5) // Extra filtering
+    {
+        continue;
+    }
+    outputList.Add(word.ToUpper(), 0);
+}
+if (File.Exists(args[1]))
+{
+    File.Delete(args[1]);
+}
+File.AppendAllLines(args[1], outputList.Keys);
