f3e3445408f61254bf774f374b7891ca82cfc41c
craigvdodge <craigvdodge@gmail.com>
2022-04-24 17:38:19 -0700

    Settings page: Moves help icon (ui consistancy) adds settings. Reloads sort order without page refresh when settings updated.

--- helple/Program.cs
+++ helple/Program.cs
@@ -1,5 +1,6 @@
 using helple;
 using Blazored.Modal;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazoredModal();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
