cead1f0758a22c17010e1342f28e3866022cc21e
craigvdodge <craigvdodge@gmail.com>
2022-05-11 14:57:25 -0700

    User defined rows and columns. Adds reset to default as well.

--- HelpleSharedWeb/SettingsModel.cs
+++ HelpleSharedWeb/SettingsModel.cs
@@ -11,8 +11,13 @@ namespace HelpleSharedWeb
 
         public SortOrder SortOrder { get; set; }
 
+        public int Columns { get; set; }
+
+        public int Rows { get; set; }
+
         public async Task<bool> LoadFromLocalStorage()
         {
+            // Sort order
             string? sortOrderAsString = await _ilss.GetItemAsync<string>("sort");
             if (sortOrderAsString == null)
             {
@@ -27,18 +32,57 @@ namespace HelpleSharedWeb
                 SortOrder = SortOrder.Alphabetical;
             }
 
-            // More settings here
+            // Columns
+            int? columnsFromStorage = await _ilss.GetItemAsync<int>("columns");
+            if (columnsFromStorage == null)
+            {
+                Columns = 4;
+            }
+            else
+            {
+                Columns = columnsFromStorage.Value;
+                if (Columns == 0)
+                {
+                    Columns = 4;
+                }
+            }
+
+            // Rows
+            int? rowsFromStorage = await _ilss.GetItemAsync<int>("rows");
+            if (rowsFromStorage == null)
+            {
+                Rows = 10;
+            }
+            else
+            {
+                Rows = rowsFromStorage.Value;
+                if (Rows == 0)
+                {
+                    Rows = 10;
+                }
+            }
 
             return true;
         }
         
         public async Task<bool> SaveToLocalStorage()
         {
+            // Sort order
             string sortOrderAsString = SortOrder.ToString();
             await _ilss.SetItemAsync<string>("sort", sortOrderAsString);
 
-            // More settings here
+            // Columns
+            await _ilss.SetItemAsync<int>("columns", Columns);
+            // Rows
+            await _ilss.SetItemAsync<int>("rows", Rows);
 
+            return true;
+        }
+
+        public async Task<bool> Clear()
+        {
+            await _ilss.ClearAsync();
+            await LoadFromLocalStorage();
             return true;
         }
 
