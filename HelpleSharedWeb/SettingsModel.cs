09fec43d00d8a26909882f6d89f9aed7c919d503
craigvdodge <craigvdodge@gmail.com>
2022-05-10 18:09:00 -0700

    Abstract settings into shared library

---
+++ HelpleSharedWeb/SettingsModel.cs
@@ -0,0 +1,53 @@
+﻿using Blazored.LocalStorage;
+
+namespace HelpleSharedWeb
+{
+    public class SettingsModel
+    {
+        public SettingsModel(ILocalStorageService ILSS)
+        {
+            _ilss = ILSS;
+        }
+
+        public SortOrder SortOrder { get; set; }
+
+        public async Task<bool> LoadFromLocalStorage()
+        {
+            string? sortOrderAsString = await _ilss.GetItemAsync<string>("sort");
+            if (sortOrderAsString == null)
+            {
+                SortOrder = SortOrder.Alphabetical;
+            }
+            else if (sortOrderAsString == "Score")
+            {
+                SortOrder = SortOrder.Score;
+            }
+            else
+            {
+                SortOrder = SortOrder.Alphabetical;
+            }
+
+            // More settings here
+
+            return true;
+        }
+        
+        public async Task<bool> SaveToLocalStorage()
+        {
+            string sortOrderAsString = SortOrder.ToString();
+            await _ilss.SetItemAsync<string>("sort", sortOrderAsString);
+
+            // More settings here
+
+            return true;
+        }
+
+        private ILocalStorageService _ilss;
+    }
+
+    public enum SortOrder
+    {
+        Alphabetical = 0,
+        Score
+    }
+}
